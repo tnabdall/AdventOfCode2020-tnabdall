027afd534a82099b040840d00c6e1c35f4f8577e
tnabdall <tnabdall@BLA>
2020-12-24 09:10:42 -0700

    Day 23 & 24 Solutions

---
+++ Day23/Program.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day23
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Part1
+            var firstNodePart1 = ParseInput(PROBLEM);
+            DoAction(firstNodePart1, 100, out var cupsListPart1);
+            PrintAnswerPart1(cupsListPart1);
+            Console.WriteLine();
+            //Console.ReadLine();
+
+            // Part 2
+            var firstNode = ParseInput(PROBLEM, addTo: 1000000);
+            DoAction(firstNode, 10000000, out var cupsList);
+            PrintAnswerPart2(cupsList);
+            Console.ReadLine();
+        }
+
+
+        private static void PrintAnswerPart1(List<Node<int>> cupsList)
+        {
+            var node = cupsList.First(x => x.Value == 1);
+            do
+            {
+                node = node.Next;
+                Console.Write(node.Value);
+            } while (node.Next.Value != 1);
+        }
+
+        private static void PrintAnswerPart2(List<Node<int>> cupsList)
+        {
+            var node = cupsList.First(x => x.Value == 1);
+            Console.WriteLine((long)node.Next.Value * (long)node.Next.Next.Value);
+        }
+
+        private static void DoAction(Node<int> firstNode, int rounds, out List<Node<int>> cupsList)
+        {
+            cupsList = new List<Node<int>>();
+            var firstVal = firstNode.Value;
+            var iter = firstNode;
+            cupsList.Add(iter);
+            do
+            {
+                iter = iter.Next;
+                if (iter.Value != firstVal)
+                    cupsList.Add(iter);
+            } while (iter.Value != firstVal);
+
+
+            var nodeByVal = cupsList.ToDictionary(x => x.Value, x => x);
+
+            var maxValue = cupsList.Max(x => x.Value);
+            var minValue = cupsList.Min(x => x.Value);
+
+            var currentCup = firstNode;
+
+            for (int i = 0; i < rounds; i++)
+            {
+                // Pick up cups
+                var pickedUpCups = new Node<int>[] { currentCup.Next, currentCup.Next.Next, currentCup.Next.Next.Next };
+
+                // Find destination cup
+                Node<int> destCup = currentCup;
+
+                int destCupVal = currentCup.Value;
+                do
+                {
+                    destCupVal--;
+                    if (destCupVal < minValue)
+                        destCupVal = maxValue;
+                } while (pickedUpCups[0].Value == destCupVal || pickedUpCups[1].Value == destCupVal || pickedUpCups[2].Value == destCupVal);
+                destCup = nodeByVal[destCupVal];               
+
+                // Remove picked up cups and place them after destination cup
+                if (pickedUpCups.First().Previous != destCup)
+                {
+                    var prevCupBeforeFirst = pickedUpCups[0].Previous;
+                    var nextCupAfterLast = pickedUpCups[2].Next;
+
+                    // Removes our 3 cups from the sequence
+                    prevCupBeforeFirst.Next = nextCupAfterLast;
+                    nextCupAfterLast.Previous = prevCupBeforeFirst;
+
+                    pickedUpCups[0].Previous = destCup;
+                    pickedUpCups[2].Next = destCup.Next;
+
+                    // Fixes sequencing of other cups
+                    destCup.Next.Previous = pickedUpCups[2];
+                    destCup.Next = pickedUpCups[0];
+                }
+
+                //Console.Write($"{i + 1}: ");
+                //PrintAnswerPart1(cupsList);
+                //Console.WriteLine();
+                currentCup = currentCup.Next;
+            }
+            
+        }
+
+        private static Node<int> ParseInput(string input, int? addTo = null)
+        {
+            Node<int> first = null;
+            Node<int> prev = null;
+            int max = -1;
+            for (int i = 0; i < input.Length; i++)
+            {
+                Node<int> newNode = new Node<int>() { Value = int.Parse(input[i].ToString()), Previous = null, Next = null };
+                if (newNode.Value > max)
+                    max = newNode.Value;
+                if (prev != null)
+                {
+                    prev.Next = newNode;
+                    newNode.Previous = prev;                     
+                }
+
+                if (i == 0)
+                    first = newNode;
+                else if (i == input.Length - 1)
+                {
+                    Node<int> last = newNode;
+                    last.Next = first;
+                    first.Previous = last;
+                }
+
+                prev = newNode;
+            }
+
+            if (addTo.HasValue)
+            {
+                var current = first.Previous;
+                for (int i = input.Length; i < addTo.Value; i++)
+                {
+                    Node<int> newNode = new Node<int>() { Value = max + i + 1 - input.Length, Previous = null, Next = null };
+                    newNode.Previous = current;
+                    current.Next = newNode;
+
+                    if (i == addTo.Value - 1)
+                    {
+                        newNode.Next = first;
+                        first.Previous = newNode;
+                    }
+
+                    current = newNode;
+                }
+            }
+
+            return first;
+        }
+
+        class Node<T>
+        {
+            public T Value { get; set; }
+            public Node<T> Previous { get; set; }
+            public Node<T> Next { get; set; }
+
+            public override string ToString()
+            {
+                var nextVal = Next != null ? Next.Value.ToString() : "None";
+                var prevVal = Previous != null ? Previous.Value.ToString() : "None";
+
+                return $"Value {Value}, Next {nextVal}, Prev {prevVal}";
+            }
+        }
+
+        const string PROBLEM = @"394618527";
+        const string EXAMPLE = @"389125467";
+    }
+}
---
+++ Day24/Program.cs
@@ -0,0 +1,460 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day24
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Part1(PROBLEM);
+            Console.ReadLine();
+        }
+
+        private static void Part1(string input)
+        {
+            // Parse input to a list of moves
+            var lines = input.Split(new char[] {'\n','\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var allMoves = new List<List<string>>();
+            foreach(var line in lines)
+            {
+                var moves = new List<string>();
+                for (int i = 0; i < line.Length; i++)
+                {
+                    if (line[i] == 'e' || line[i] == 'w')
+                        moves.Add(line[i].ToString());
+                    else
+                    {
+                        moves.Add(line[i..(i + 2)]);
+                        i++;
+                    }
+                }
+                allMoves.Add(moves);
+            }
+
+            Dictionary<(int x, int y), bool> flippedTiles = new Dictionary<(int x, int y), bool>();
+
+            foreach(var moves in allMoves)
+            {
+                int x = 0; int y = 0;
+                foreach(var move in moves)
+                {
+                    switch (move)
+                    {
+                        case "e":
+                            x += 2;
+                            break;
+                        case "w":
+                            x -= 2;
+                            break;
+                        case "ne":
+                            x++; y++;
+                            break;
+                        case "nw":
+                            x--; y++;
+                            break;
+                        case "se":
+                            x++; y--;
+                            break;
+                        case "sw":
+                            x--; y--;
+                            break;
+                        default:
+                            throw new Exception();
+                    }
+                }
+
+                if (flippedTiles.TryGetValue((x, y), out var flipped))
+                    flippedTiles[(x, y)] = !flipped;
+                else
+                    flippedTiles[(x, y)] = true;
+            }
+            Console.WriteLine(flippedTiles.Count(x => x.Value));
+
+            for (int days = 0; days < 100; days++)
+            {
+                var xMin = flippedTiles.Keys.Min(x => x.x);
+                var xMax = flippedTiles.Keys.Max(x => x.x);
+                var yMin = flippedTiles.Keys.Min(x => x.y);
+                var yMax = flippedTiles.Keys.Max(x => x.y);
+
+                List<(int x, int y)> tilesToFlip = new List<(int x, int y)>();
+
+                // Check every tile from min - 2 to max + 2
+                for (int i = xMin - 2; i <= xMax + 2; i++)
+                {
+                    for (int j = yMin - 2; j <= yMax + 2; j++)
+                    {
+                        bool isFlipped = flippedTiles.TryGetValue((i, j), out var flipped) && flipped;
+                                                
+                        var neighbourCoordinates = new (int x, int y)[]
+                        {
+                            (i - 2, j),
+                            (i + 2, j),
+                            (i - 1, j + 1),
+                            (i - 1, j - 1),
+                            (i + 1, j + 1),
+                            (i + 1, j - 1)
+                        };
+
+                        var neighbourFlippedTilesCount = neighbourCoordinates.Count(x => flippedTiles.TryGetValue((x.x, x.y), out var flipped) && flipped);
+                        if (isFlipped && (neighbourFlippedTilesCount == 0 || neighbourFlippedTilesCount > 2))
+                            tilesToFlip.Add((i, j));
+                        else if (!isFlipped && neighbourFlippedTilesCount == 2)
+                            tilesToFlip.Add((i, j));
+                    }
+                }
+
+                foreach(var tile in tilesToFlip)
+                {
+                    if (flippedTiles.TryGetValue((tile.x, tile.y), out var flipped))
+                        flippedTiles[(tile.x, tile.y)] = !flipped;
+                    else
+                        flippedTiles[(tile.x, tile.y)] = true;
+                }
+            }
+            Console.WriteLine(flippedTiles.Count(x => x.Value));
+
+        }
+
+        const string PROBLEM = @"weseswseswesewseseseswewswseswnenwsese
+nwswwwnwwwswswseswseswwsewswnewsw
+wnwnwsewwnwenwnwnwwwnwnwnewnwswne
+nwnwnwewnwnwswwnenwsenenwnwnwnwenwnwnw
+swswewswewneseeseswseewwsewnwne
+swwnenenenwswsweneswswswswneewwsewnw
+neswewseswswseswswswswneswwswsenewsese
+nwwnenenwwnwnewnwsenenwsenwse
+eswwnwswswswwneswwwwewnwnwswsewse
+sewseneeesesewseseswnesesewseseeese
+nwweneseeswnesewsesenwsese
+swswswneswswswswwsw
+nwnwnwnwnwswnwwnwnwnenwewnwsenwnwese
+enwwneneseseswewneeswsenwneswneseenw
+enwswesenwnwseenwseseeeswnwnwneseswnwe
+eswwwswneneesenwenenenwswswnese
+wnwweseswnwneenenewswwnweswwsesw
+nwneswneeeswewwneseneseswswwwsenenw
+nwweweeeswnwswesweneenweneeew
+swseswswwwseswwwwwwwwnwewwne
+nwsewsenwnwwwnenw
+neswswneeeeeeneenwwnwneewseswwse
+seesesewseswenwsesewneeenenwswnesew
+eeenweeesweeeee
+swnwnwnwswsewnenewswwwweswwsewswe
+seneenwnwwnwswwnwnwnwwnwnwewwnwnw
+swswneenenwwewswnwwswnewwnwswwww
+neswenenwnwnenenenesenwswswnweswwsewnenw
+eeseeeenweeseesesenwe
+nenwneneneewneesenesenenenene
+eenesewseseewe
+nwnwwwwwwswswwneweeewswwwww
+wwnesenweseneneeeneswswswnesenwnwe
+swseseswenwsesesenweeswsesesewswnwnwnw
+newswnweswswseswnenenene
+neneeneneeeneeeswneeneesewwnwsenw
+neeneswwnwnenwwneenwneseswneenewe
+nenwswseswneswswswneswswswswwsewsweseww
+sesewseeseesesesenewsenwnwswewsenew
+swswneeseswswwwwswwneswswnwswswswsenw
+swswswseswswneswswseswseswsweww
+swwneswneswwseseenwswswswswswsenwswswsw
+seswseesenwewsesesw
+sweneswnwenewneseneenenwnwneseswseenw
+nwnenenwnwnewenenwsweswne
+wwnewwwwwwewewnwwwwnesewse
+nwnwsewnwnwweswnenwnw
+swneeneneeneneneneeenenee
+eneweseeesenwwweeeeeeweene
+nenwnwswnwnwnwnwnwnwnwnwnwnenwnwsese
+nwneneesweneeeneenenene
+eneesweneneenenwneesenwneneeneswnw
+seswnwswneswswnwswswseseseswsesenwseesese
+swwenwenwnwnwnwnwswwnwneswwnwneeenw
+wseeseneneswwwnewnenenenwswwswswwsw
+eneenwnwnenwwnwnwnwnwnwswnwnwnwnwsew
+nwnwswswnwnwnwnwweweenwwnwnwnew
+swenwsenenwswwneenenenwswnenenenenwnenenw
+swnenwwneseswswwswneswwnewswswesenw
+nenenenenwnwnesenwswnwenenwnenwenwneswne
+wenenweeswsweswnwseswneeneneswswnw
+seesesesesweseneswswswwnwwswsenewswsw
+newwwsenwnwswwwnweswnwnewnwwsenese
+senwwnwnenwnenenwnenenwnweenwnewwnw
+eneswwseenenwnenwswswseeneenesesew
+eseeeswwenwenweswseneeenenenene
+seeswnweeswneeeeneswneneneneeeew
+sewswwswwswneswseswneswnenwwswswseww
+seseenwseswsesenwnwnwwseseesesese
+swswseneswneswswswswsweswswswwswswnwsw
+neeseeseneneenweewe
+swwsenenenenwneneneneneneneeneneneswne
+enwnenwnwneewswseenenenwswswnwnwenwnwsw
+neswenwneeeeeeee
+neesenesewswnwswsesewswseswsese
+nwnwnwnewsenwnwsenesenwneswnewnenwnenw
+eneswsenenenewene
+wsenwnwnenwnwswwnwnwenwnenwswsenewwnw
+nenewswneswsenwenwnwnesenwnwneeswneene
+swneseseneswnesenesewwswesewseseswse
+eneeewnewwwswnewesenwseseweswsene
+neesweneswswneeeswneneeeeneenenw
+wsenwnwwnwnwnwnwwwwww
+sewwsewswwwnwwnwenwwnewwnwnwe
+neeneeneeswneeeneee
+swseneseneseseswnenwswneswenenwseswww
+seseswswswwsewswswnewsesweneswnwswesw
+seswswsenwswneswswswnwwsewnwwwswwse
+swsenwswnwwswswnwsenwneneenesenwnenee
+swnwswsewswswnewswswswswseswswswseswe
+neeeeneneneneneweeene
+sesweneseswnwneswswnwwnwneewenesee
+sewswsesenesewseseseneswseseswnesesesw
+nwswwswwswswswswswwneswswwswe
+enwnewnwswnenenwwseswnwswsenwnenwene
+esesesenweseeseseseeenwseseseswsenw
+neeneneneswseewneswnwnenwnwnesenenesee
+swswwwswswswswesww
+wwwswnwswswenesewenweewneenwene
+eseeenewwwwnenenewwneneneseewsese
+wswsesweswswswnewswnw
+neeeeneenesweeeswswneeeenwsenw
+sesesenewnwswsenwnwnwwnwnwwwwseswsese
+seeesesweeeseeenwenwsweneseewee
+wneweswswwneenwwwsenewsewwwsw
+nenewneneneneneenenenesewnenenene
+seswswnwnenwnwnenwnenenenewnesweeswse
+swesesesewnwseesenwswnwe
+swewswwesesewswwnwweneenwswsww
+seeswewswnwseeseseseesenwnwnwenwseese
+neneswswwwseneswwneswswswswwswsesesw
+seenwweeswenesweneeeneeeenweene
+swwesenwswwenenwneewwsesww
+nwwnwneeenwswnwenwnewwwnwnwswnwnw
+neswneneneneneeneeenwneneswnenewnesene
+eseseenesenwswsesenwseseeseseeswnese
+neseswswenwnewneneswnenwneseswsweswsww
+swsenwseneswswnesweeswswswswswswswswsww
+eseeewseeeseneseeeseeewneseww
+nwnwswwnwnwnwnwnenwwww
+wwwwwnwnwwwneseww
+seeswseneseeeseseseswwneeeseesesenw
+wnwewnwnwwwnesewwnwnw
+eesewsenwsesenesee
+sesesewesweneewnweneesesese
+nwseewseneswseseseeseewsenenweswese
+wneswwswnenwwswseeswwwnw
+nesesesewneseswswwswswswswesenwnwnwsesw
+nwnwnenwnwnwnewnwwswnwewnwnwswenwnw
+swseeswwnwnwsenenenwswwswseseswswsesesesw
+swseseseeneewsesesewneesewseseeese
+sewwsesenwwewnenenwneneenewnwnwe
+nwwenwwnwnenwnenwnwnwsene
+nwenwnwewnwwesesenwswwnwnenwwwne
+senwsewwnwwwwwseenwswwnwwneese
+wwwswwnwswsewewewwnewewswsww
+swneeeenenwnwneswese
+neswwwwwnweseenwneenwswswseeswnw
+eswnenwswneeeswseneswneenenwnenwee
+nwnewneswswwneeseseseswseneeneeseesw
+neeneeeeweneswenweseswee
+eseswnwwswnwsweseseswseswswsesesweswsw
+seenwenwseswsesewweseseseweswse
+eenweseswewneswwnwwenwwwenwsw
+swseseswwwswwnenewwswswnwnesesw
+wwnwswwswsewneneewnwnwwswnwwwwse
+nenenesenenwnwnenenw
+nenwwneneseneneswneswnenenweneneswne
+nwsenewwnwnwnewwwswswswseeswwwswsee
+swnwnwnwswneseeenesweeewesweeeee
+swseseenwswswswwnwswwswsweswse
+nwswnenenwneswswnenenwswnwneswswnweneneene
+swswenwsenesewswswnwwenwswnwneswswswsw
+wwswswnwwneswswswneeswsw
+nwsweswwwwswwswwnewswweswsewswne
+seewseswswnwwswsenenesweeswswwswnwnenw
+neenwswseeswswsweseeneneeneewsewe
+weenwsesesenwnwseseswseseseswnesesee
+seswswseswswswnewsesw
+nwnwnwwnwnwnwnwnwenwnwnwnw
+neswneswseneswneneneneneneneewneneenw
+swwnwneswnwswswwswswswsesewneeswesee
+nenwnewnesenwswnwnenwnenwe
+esenwswsesenesewseswseseswseswsw
+eseweeeeewneeeeeeewene
+seswnwneswswwseswsewsweseswswswseswse
+nwwnwewwnwwnwswnww
+nwseseswseswswswswswsweswswsw
+enesenesenewswsenwneswnenwnewnwnwnenwnw
+seswswnwneswsweswswswswswnwswswweswswesw
+wseswswwwwsenenesweseeseswse
+nwnwnewwenwnwswseew
+nwwnwsenwnwnwnenesenwnenwnwnenwnwnwnwse
+wwnwsewnewwsewnwnenwsenwwwwnwww
+wwwewwwwwwwwwwwswnwe
+seseesweenwenwsenwswsenwseeeseseswese
+swwenwnwnwnwnwnwwnwnwnww
+nwnenwneneseneneneswenenenenwnwsenwnew
+nenwsenwnenenesenenenenenenwnwnwneseww
+nwwnwnenenesenenenesenenwnenenenesewe
+neneewneswnwsewnenenwnene
+nenwneewswnwnwneneewneneneenwnenenw
+nwwwnwwnwnwseswnwwwsenwwneswnewewne
+esweneenenwsewwswswswswwseswsewnwne
+wseenwseswnesenwse
+eeeswenwesenesenwewswne
+neswseseseneswseswseseneswseseswsenesesese
+wsewewwnewwnenwsenewwwwswswsw
+wneeeswewsesenenwsewnwsewnewseese
+swnweewnwwseeswnenwseswneswwnesese
+sesenwewsesewswsweneneseeseeesene
+wnwenwwwnwewsewwwwswsewwww
+seneenenwnweeeeeeneeseenwnwswse
+wsenewwwwwnwwwnwnwnww
+nenwnwenwwwnenewwneenenwesenene
+seeeswsweseesewenweeseseeneenee
+sesewenwseeeeseewswnweeeseeenw
+swenwnwswnwnwseeneswnwnene
+swswswwswneesewswwwneeneeweswsw
+swwnweeswnesenenweseseeesesewseese
+sweseswwwswwneenwwwswe
+wneneneeesenenenwneseeneneneeneswne
+swwesesewneswwenwwswwwwwwwwnwsw
+swenwswswswswswwwwnwnwnwswewswswee
+senwswnwseeeneeswse
+swswnwesweswswsweswswswwswnwnwne
+neswneeneneneneneneeneene
+eneseeseeswenweeweseseswseeenese
+seswnwseseswswenesenenesewsewswswswse
+wsesenwneseseseew
+swwwswwewswwswsww
+nwswswnwswswsweswsweswswewsenwswnene
+eeeeeewnweeeweeseeswnwneseee
+nwwwnwnwwsenwnenwswenwnwnwnwnwnenwnwe
+seswsenwnwwseeseseneeswswswswswswnenw
+swwneeeeenenweswe
+wwwwnwwwnwswnenwsewwewww
+swnenwnenwnwnewswnwnwenenw
+seewswsesenwswneeee
+nwwwewwwenwwwwweswwwnwnwnwse
+wwnwwwswnwewwwswwswnwenewww
+seswnenwneswnwsweenwnweenwwwweswnw
+neswneseenesweenenenew
+nenenwwneneneeeneneenenwswewesweene
+swswswswswneswswswnwswseswswwneeswesw
+seeweeeeseseeneeswnwewnwesee
+swnwewswswnwswneswswswsesweneswswswsw
+swnewneswnwswseswswswswsweswswswneswsw
+swnwswneneneewnwwenwse
+sewwwnewwswwwwwsw
+swnwswnewnwsesewsewnenwnwsenwsenwenw
+nwnwnwnwsenwsenwnwnwnwnenwnenwnwswneswnwnw
+senesesesesesesewsenwswseseneseeseswwse
+sesenenweneseseseseswwnewseewsenww
+wneneswnenenenenenesenenenenenenesenwnw
+swswneenwnwnwnwwnwnwnwnwnenwnwsenwsenwnw
+wwnwwneswswwwswswswswswesew
+wwnwseswnwnewwswwwewsewwsewene
+wswseeswswswswswnwwneeswswswwwswswsw
+wwnewesesewwseenwnwswnwnwnwnwnwse
+neeneswewwswneswnenenenweenewnenwsee
+swenwnwsesweenweeeneneneswewseese
+sewwseweseseeseseneseesesenwnewse
+nenenwnwneswneneneneseeneneneswnenene
+newseeeneneeeeneeneenenw
+neeneneswswnwnwneneneneseneesewnwnene
+seseenwseseeswseneseswsenwseesesesee
+swswswsesenwswnwswseswneswseswswswseseswnw
+eneneneneenwesweweeeeswwenese
+enenwweeneneneneswswnenenenenenenene
+sewswswnwswswneseseneneswswseswseswswsw
+sesesenwwnwnwwnwwwwenenwwnww
+seseseswswswseenwseseswsweseenwnwsesw
+wnwswseswsenwsesenwnwesesweseesenenese
+sesesenwseseseswseseseneseesewseswnww
+seswseeseswnwnwseewenwswseeseswseswse
+seseswseneswneenwwseesenewswnwswwswse
+wnwneneseswnenewnwnenwneneenenenewsenene
+neneseseeeewwewsweeenwnwneswee
+nwwwewwewww
+newnwswwesesewnenwneswnewnwseneesew
+nwwwwewewwswwnenwsenwnwnwnwwww
+neseneswnenwweeneseewswseeneeew
+newwsenwsesesesweseewseseseneseseew
+neneneswewneneneneeeewnenesene
+enenwswseeneseseeseweseee
+wnenewwswewseeswswwnwwswwwwsw
+seswnwnweenwsweenwneseeneeenenwneswe
+newwwwswswneswwwswsww
+wwwwsenwswwnwewneswwww
+neswnenwnenenwnenwswnenenwswnenwnw
+wsesenenwnenwnwnwnwneswwnenenwnwnwene
+neswswwwwnewswswwswswwnwsenewnwswee
+eswwsenenwneeeseseweeeswswenese
+newseenenweesweeseesewsesenwseesw
+neswswsenesweseswwwwswsweeswsenwswsw
+swnwneswnenewenenenenenwenwneneenesw
+esweswswnwswswsewnweeewwwseeswnw
+nwnwwseweswnwswnwwewnesewwnenenwnw
+senwnwsenwnwnwnwseswwnwnenwnwnwnwenenw
+swnewswwwwwnewwwwnwneewwwsew
+neneneeeseneneneesewswewneeenee
+wswsenewneenesewnenwnenwsenwneswswnene
+neewwnwnenwnenesesenenwnenwsenenwneswnee
+seswnwneenesenwnenenwswnewneneneenee
+nwnwwnenwswwwnenwnwnwneswnwswnwnwsenwnwne
+swwwnewsenesenwsenewseeswnenesenwwsw
+senwseseseneswseenwwwse
+seseswseseseseseseswswseenwswsesenw
+nenesenenwswwsewnwswnwneneeewnwene
+swswswwswswswswswswwneswsw
+sweneseesenwesewnewnwseeseswnwesese
+nwnwswswswseswswsweswsw
+eswswwswswwnwswwswwswswneswswnwsesw
+newwwwwwwewwsenwnenwewsenenwsw
+nweswweenwnwswnwenwwnwswnwnwnwnwnwnw
+senwwswnenwseswswswnewneeswwwww
+senwseswswnweneeswseswswwseswneswswswsw
+weswwsewnweeewweneswnesewsene
+neeswswnwnwnwswnwswneewnwnwnwnwswnenww
+nwswwneewnesenesenenwnenenenwnwnwnwnenw
+sweseeneseeneswenwneswswnwesee
+nesewnwnwwnwnwnwnwnwnwnwnenwnwsenwnw
+newneeenweeeeneneseene
+wesesenwneenenwneswwneneenweswsene
+eswnwswnwsenesweneswswsweswswsenwswsesw
+swsesenwseswswseswswewsenwswsesene
+weseeseeswswweseeneseseneeseenwne
+wswswswswwsenwswnewneswneswswwsewwsw
+eseswnwesenwnwwnwswnwsweneeeneesesew
+wnwwswswwwnewswsenwswswsesweswnww
+eeeneeneswewseeeneeweneesww
+wnenwswwseewwswnewwnewwsenwnwsew
+nenwswenenwsenenwsenwnwnwnenewnenenwswsw
+neswnewweeeeeeeneneenenenenwsee
+ewnweeeeswswseeeenweeeeenwe";
+
+        const string EXAMPLE = @"sesenwnenenewseeswwswswwnenewsewsw
+neeenesenwnwwswnenewnwwsewnenwseswesw
+seswneswswsenwwnwse
+nwnwneseeswswnenewneswwnewseswneseene
+swweswneswnenwsewnwneneseenw
+eesenwseswswnenwswnwnwsewwnwsene
+sewnenenenesenwsewnenwwwse
+wenwwweseeeweswwwnwwe
+wsweesenenewnwwnwsenewsenwwsesesenwne
+neeswseenwwswnwswswnw
+nenwswwsewswnenenewsenwsenwnesesenew
+enewnwewneswsewnwswenweswnenwsenwsw
+sweneswneswneneenwnewenewwneswswnese
+swwesenesewenwneswnwwneseswwne
+enesenwswwswneneswsenwnewswseenwsese
+wnwnesenesenenwwnenwsewesewsesesew
+nenewswnwewswnenesenwnesewesw
+eneswnwswnwsenenwnwnwwseeswneewsenese
+neswnwewnwnwseenwseesewsenwsweewe
+wseweeenwnesenwwwswnew";
+    }
+}
